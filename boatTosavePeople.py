d8326d440248660d0a9e9e4d72a048db253aa944
Metyme1 <millionmety@gmail.com>
2023-11-14 11:49:12 +0300

    nov-14-2023

---
+++ boatTosavePeople.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        left = 0  
+        right = len(people) - 1 
+        boats = 0 
+        
+        while left <= right:
+           
+            if people[left] + people[right] <= limit:
+                left += 1 
+                right -= 1 
+            else:
+                right -= 1 
+            boats += 1 
+        return boats
