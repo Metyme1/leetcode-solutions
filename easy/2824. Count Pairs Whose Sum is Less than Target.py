7dd41e6000cbb706471e3ef92a5e8f339db00b47
Metyme1 <millionmety@gmail.com>
2024-02-14 10:24:58 +0300

    feb 13 , 2024

--- easy/2824. Count Pairs Whose Sum is Less than Target.py
+++ easy/2824. Count Pairs Whose Sum is Less than Target.py
@@ -13,3 +13,19 @@ class Solution:
 
 
 #  linear solution
+
+
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        nums.sort()  # Sort the array
+        left, right = 0, len(nums) - 1
+        count = 0
+
+        while left < right:
+            if nums[left] + nums[right] < target:
+                count += right - left  # Add all pairs between left and right
+                left += 1  # Move left pointer to the right
+            else:
+                right -= 1  # Move right pointer to the left if the sum is too big
+
+        return count
